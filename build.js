fb32a71ae4aeabbb349a9be783b1a04aa7949157
Anton Khlynovskiy <subzey@gmail.com>
2014-09-03 13:10:16 +0400

    5.43K: Mangle toplevel; use another uglifyjs module (why does it have two?)

--- build.js
+++ build.js
@@ -1,26 +1,54 @@
 #!/usr/bin/env node
 var fs = require('fs');
-var path = require('path')
+var path = require('path');
+
+var keep = ['start', 'startDemo', 'load'];
+var mangledNames = {};
+
 var html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
 html = html.replace(/(<script[^>]*>)([^]*?)(<\/script>)/g, function(_, openTag, content, closeTag){
 	var uglifyjs;
 	try {
-		uglifyjs = require('uglifyjs');
+		uglifyjs = require('uglify-js');
 	} catch (e){
 		console.error('Could not import uglifyjs. install is with `npm install`!');
 		process.exit(1);
 	}
 
-	return openTag + uglifyjs.minify(content, {
+	var keepCode = keep.map(function(varname){return ';window.__exported_' + varname + '=' + varname;}).join('');
+	// Yes, I don't know how to set mangle=toplevel
+	content = 'function __main(){' + content + keepCode + '}';
+
+	var minified = uglifyjs.minify(content, {
 		fromString: true,
-		drop_console: true,
-		cascade: true,
-		if_return: true
-	}).code + closeTag;
+		compress: {
+			drop_console: true,
+			drop_debugger: true,
+			cascade: true,
+			if_return: true
+		},
+		mangle: true
+	}).code;
+
+	// strip iife
+	minified = minified.replace(/^[^]*function\s+__main[^]*?{(.*)\s*};?$/, '$1');
+	// get minified names
+	minified = minified.replace(/[;,]window.__exported_(\w+)\s*=\s*(\w+)/g, function(_, originalName, mangledName){
+		mangledNames[originalName] = mangledName;
+		return '';
+	});
+
+	return openTag + minified + closeTag;
+});
+
+html = html.replace(/(onclick=)((["']).*?\2|[^>\s*]*)/g, function(_, attr, onclickStr){
+	for (var originalName in mangledNames){
+		onclickStr = onclickStr.replace(new RegExp('\\b' + originalName + '\\b', 'g'), mangledNames[originalName]);
+	}
+	return attr + onclickStr;
 });
 
 try {
 	fs.mkdirSync(path.join(__dirname, 'build'));
 } catch (e){}
-
 fs.writeFileSync(path.join(__dirname, 'build', 'index.html'), html);
